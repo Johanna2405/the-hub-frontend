9ee6dc09409ff61232f880a6a5f256627471a807
Anne Martinez <martinezannemerlyn@gmail.com>
2025-03-26 12:05:46 +0100

    initial files

---
+++ src/components/Messaging/MessageList.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import MessageBubble from "./MessageBubble";
+
+const MessageList = () => {
+    return <MessageBubble />;
+};
+
+export default MessageList;
---
+++ src/pages/MessagePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MessageList from "../components/Messaging/MessageList";
+import MessageInput from "../components/Messaging/MessageInput";
+
+const MessagePage = () => {
+    return (
+        <>
+            <MessageList />
+            <MessageInput />
+        </>
+    );
+};
+
+export default MessagePage;
