2f4b5bc46579b376294f9937b28eedd4e6d8a98a
Alexander  von der Bey <alexandervonderbey@gmail.com>
2025-04-08 11:46:08 +0200

    standardized api calls and backend_url

--- src/utils/listsAPI.js
+++ src/utils/listsAPI.js
@@ -1,25 +1,27 @@
 import axios from "axios";
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
-const LISTS_API = `${BACKEND_URL}/api/lists`;
+const API = axios.create({
+  baseURL: `${BACKEND_URL}/api`,
+});
 
 // ------- LISTS -------
 
-// GET all lists per id
+// GET all lists per user ID
 export const fetchListsPerUserId = async (userId) => {
-  const response = await axios.get(`${LISTS_API}/${userId}`);
+  const response = await API.get(`/lists/${userId}`);
   return response.data;
 };
 
 // GET list by ID
 export const fetchListById = async (id) => {
-  const response = await axios.get(`${LISTS_API}/${id}`);
+  const response = await API.get(`/lists/${id}`);
   return response.data;
 };
 
 // POST new list
 export const createList = async (listData) => {
-  const response = await axios.post(LISTS_API, listData, {
+  const response = await API.post("/lists", listData, {
     headers: {
       "Content-Type": "application/json",
     },
@@ -29,7 +31,7 @@ export const createList = async (listData) => {
 
 // PUT update list
 export const updateList = async (id, listData) => {
-  const response = await axios.put(`${LISTS_API}/${id}`, listData);
+  const response = await API.put(`/lists/${id}`, listData);
   return response.data;
 };
 
@@ -37,33 +39,30 @@ export const updateList = async (id, listData) => {
 
 // GET all items in a list
 export const fetchListItems = async (listId) => {
-  const response = await axios.get(`${LISTS_API}/${listId}/items`);
+  const response = await API.get(`/lists/${listId}/items`);
   return response.data;
 };
 
 // GET specific item in a list
 export const fetchListItemById = async (listId, itemId) => {
-  const response = await axios.get(`${LISTS_API}/${listId}/items/${itemId}`);
+  const response = await API.get(`/lists/${listId}/items/${itemId}`);
   return response.data;
 };
 
 // POST new item to a list
 export const createListItem = async (listId, itemData) => {
-  const response = await axios.post(`${LISTS_API}/${listId}/items`, itemData);
+  const response = await API.post(`/lists/${listId}/items`, itemData);
   return response.data;
 };
 
 // PUT update specific item in a list
 export const updateListItem = async (listId, itemId, itemData) => {
-  const response = await axios.put(
-    `${LISTS_API}/${listId}/items/${itemId}`,
-    itemData
-  );
+  const response = await API.put(`/lists/${listId}/items/${itemId}`, itemData);
   return response.data;
 };
 
 // DELETE specific item from a list
 export const deleteListItem = async (listId, itemId) => {
-  const response = await axios.delete(`${LISTS_API}/${listId}/items/${itemId}`);
+  const response = await API.delete(`/lists/${listId}/items/${itemId}`);
   return response.data;
 };
