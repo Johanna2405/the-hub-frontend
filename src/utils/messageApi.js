28cf1bd637e68037b1051d91a6175e9b8acefcd8
Anne Martinez <martinezannemerlyn@gmail.com>
2025-04-10 21:54:46 +0200

    updated hardcoded url

--- src/utils/messageApi.js
+++ src/utils/messageApi.js
@@ -1,21 +1,5 @@
-import axios from "axios";
 import { io } from "socket.io-client";
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
-
-const API = axios.create({
-  baseURL: `${BACKEND_URL}/api`,
-});
-const socket = io("http://localhost:8080");
-
-export const fetchAllMessages = async () => {
-  try {
-    const response = await API.get("/messages");
-    return response.data;
-  } catch (error) {
-    console.error("Failed to fetch messages:", error);
-    throw new Error("Failed to fetch messages. Please try again later.");
-  }
-};
+const socket = io(import.meta.env.VITE_BACKEND_URL);
 
 export const setupChatListener = (setChat, setTypingUser, setOnlineUserIds) => {
   socket.off("receive_message");
