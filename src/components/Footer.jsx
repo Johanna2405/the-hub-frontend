6ebc8be024944dea5cca4000fa899fcac68ca986
Johanna <johanna.k.1996@googlemail.com>
2025-04-14 11:54:20 +0200

    landingpage refactor

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,14 +2,12 @@ import { Link } from "react-router";
 
 const Footer = () => {
   return (
-    <footer className="bg-primary w-full px-4 pt-8 pap-8 min-h-[33vh]">
-      <div className="flex flex-col md:flex-row items-center justify-between border-b-1 border-ultramarine">
+    <footer className="bg-primary w-full px-4 pt-8 pap-8 min-h-[33vh] flex flex-col justify-between">
+      <div className="flex flex-col md:flex-row items-center justify-between">
         <div className="p-8 flex flex-col items-center md:items-start gap-4">
           <Link to={"/get-started"}>
-            <img src="./logoipsum-329.svg" alt="Logo" />
+            <img src="./hub-dark.svg" alt="Logo" className="w-40 lg:w-56" />
           </Link>
-          <span className="font-bold">the hub</span>
-          <span>contact information here</span>
         </div>
         <div className="flex gap-12">
           <div className="p-8 flex flex-col gap-4">
@@ -60,7 +58,9 @@ const Footer = () => {
         </div>
       </div>
 
-      <p className="text-center py-4">© 2025 the hub | All rights reserved</p>
+      <p className="text-center py-4 border-t-1 border-ultramarine">
+        © 2025 the hub | All rights reserved
+      </p>
     </footer>
   );
 };
